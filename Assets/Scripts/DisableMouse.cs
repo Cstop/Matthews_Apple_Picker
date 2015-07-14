ea8a17294c68a8a266d54b0bd9658e6ab14341a1
Corey Matthews <cstop1986@gmial.com>
2015-07-14 14:03:14 -0400

    Apple Picker Game
    Advance Game Prototyping assignment week one. Catching Apples for
    points.

---
+++ Assets/Scripts/DisableMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+	
+}
